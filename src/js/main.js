c31311ca9b2a05680f0c99de3ba183cb2c5deff8
Roy Bron <roy@slik.eu>
2018-02-27 15:11:29 +0100

    Page: Member

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,9 @@
         .on('click', '.js-toggle-grandchildren', function(){
             $(this).toggleClass('menu__toggle--is-active');
 			$(this).next('.js-menu__grandchildren').slideToggle();
+        })
+        .on('click', '.js-member__read-more', function(){
+            $(this).next('.js-member__expandable').slideToggle();
         });
         
 })(jQuery);
