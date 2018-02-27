0d0ee3464c5b0e6909eb2d2da3fb596e7f961e91
Roy Bron <roy@slik.eu>
2018-02-27 16:36:21 +0100

    Page: Accordion

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 (function($){
 
     $('body')
+        // Navigation
         .on('click', '.js-header__menu', function(){
             $('.js-navigation').slideToggle();
         })
@@ -12,8 +13,14 @@
             $(this).toggleClass('menu__toggle--is-active');
 			$(this).next('.js-menu__grandchildren').slideToggle();
         })
+        // Member
         .on('click', '.js-member__read-more', function(){
             $(this).next('.js-member__expandable').slideToggle();
+        })
+        // Accordion
+        .on('click', '.js-accordion__definition', function(){
+            $(this).toggleClass('accordion__definition--is-toggled');
+            $(this).next('.js-accordion__description').slideToggle();
         });
         
 })(jQuery);
