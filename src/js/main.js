8b2975f06cd41678bed80cc8034b896a466baf57
Roy Bron <roy@slik.eu>
2018-02-28 13:40:34 +0100

    Single: Social Share

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,25 @@
         .on('click', '.js-accordion__definition', function(){
             $(this).toggleClass('accordion__definition--is-toggled');
             $(this).next('.js-accordion__description').slideToggle();
-        });
+        })
+        // Social Share
+        .on('click', '.js-share__link', function(e) {
+			e.preventDefault();
+			windowPopup($(this).attr('href'), 974, 682);
+		});
         
+
+    // Responsible Social Share Links enhancement by https://jonsuh.com/blog/social-share-links/
+	function windowPopup(url, width, height) {
+		
+		var left = (screen.width / 2) - (width / 2),
+			top = (screen.height / 2) - (height / 2);
+	
+		window.open(
+			url,
+			"",
+			"menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+		);
+	}
+    
 })(jQuery);
