b2fa58615c8c1e978ca938a424f9ee14fb17e216
Roy Bron <roy@slik.eu>
2018-03-05 17:20:30 +0100

    Agency Single: Info list

---
+++ agency-single.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Agencies | Single
+ *
+ * Methods for TimberHelper can be found in the /functions sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+$context['post'] = new TimberPost();
+
+Timber::render('agency-single.twig', $context);
