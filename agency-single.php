fd143f292025f0b8d4a1ccd3d9c01c239ee65eb9
Jurgen Oldenburg <info@jold.nl>
2018-03-22 21:09:27 +0100

    Add countries to single agency view.

--- agency-single.php
+++ agency-single.php
@@ -11,25 +11,39 @@
 
 include( get_template_directory() . '/classes/RestClient.class.php');
 
-$context = Timber::get_context();
-$context['post'] = new TimberPost();
+$context            = Timber::get_context();
+$context['post']    = new TimberPost();
 
 $variable = $_GET['id'];
 
 if ( isset($variable) && !empty($variable) ) {
 
     $api = new RestClient([
-        'base_url' => 'https://backend.deqar.eu/webapi/v1/browse/agencies/' . $variable . '/?history=false',
-        'format' => "json",
-        'headers' => ['Authorization' => 'Bearer 03a64a808705672b4fc61f0da1fe28d430250cd4' ],
+        'base_url'  => 'https://backend.deqar.eu/webapi/v1/browse/agencies/' . $variable . '/?history=false',
+        'format'    => "json",
+        'headers'   => [ 'Authorization' => 'Bearer 03a64a808705672b4fc61f0da1fe28d430250cd4' ],
     ]);
 
-    $result = $api->get('');
+    $countries = new RestClient([
+        'base_url'  => 'https://backend.deqar.eu/webapi/v1/browse/countries/by-agency-focus/' . $variable . '/?limit=999&offset=0',
+        'format'    => "json",
+        'headers'   => [ 'Authorization' => 'Bearer 03a64a808705672b4fc61f0da1fe28d430250cd4' ],
+    ]);
 
-    if($result->info->http_code == 200) {
-        $context['agency'] = $result->decode_response();
+
+    $rApi           = $api->get('');
+
+    if($rApi->info->http_code == 200) {
+        $context['agency'] = $rApi->decode_response();
     } else {
         $context['agency'] = false;
+    }
+
+
+    $rCountries     = $countries->get('');
+
+    if($rCountries->info->http_code == 200) {
+        $context['agency']->countries = $rCountries->decode_response()->results;
     }
 
 } else {
@@ -37,5 +51,7 @@ if ( isset($variable) && !empty($variable) ) {
     $context['agency'] = false;
 
 }
+
+
 
 Timber::render('agency-single.twig', $context);
