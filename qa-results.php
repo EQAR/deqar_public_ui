61e51473a5dbe50378fe8b21a51a9e02bdf065a5
Jurgen Oldenburg <info@jold.nl>
2018-03-22 23:46:40 +0100

    QA Results: Show institutions by country (needs work).

--- qa-results.php
+++ qa-results.php
@@ -9,7 +9,14 @@
  * @since    Timber 0.1
  */
 
+require_once( get_template_directory() . '/classes/EqarApi.class.php');
+
+$eqarApi = new EqarApi();
 $context = Timber::get_context();
-$context['post'] = new TimberPost();
+
+$context['post']                    = new TimberPost();
+$context['institutions']            = $eqarApi->getInstitutions();
+$context['countries']               = $eqarApi->getCountries();
+$context['institutionsByCountry']   = $eqarApi->getInstitutionsByCountry();
 
 Timber::render('qa-results.twig', $context);
