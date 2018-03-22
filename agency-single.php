640b2303c0ab79d64895aa00905c9d21bd6b6030
Jurgen Oldenburg <info@jold.nl>
2018-03-22 21:19:50 +0100

    Code cleanup and improve code documentation.

--- agency-single.php
+++ agency-single.php
@@ -14,34 +14,38 @@ include( get_template_directory() . '/classes/RestClient.class.php');
 $context            = Timber::get_context();
 $context['post']    = new TimberPost();
 
-$variable = $_GET['id'];
+// Get the agency ID from the GET variable.
+$agencyId = $_GET['id'];
 
-if ( isset($variable) && !empty($variable) ) {
+// Check if the agency is set.
+if ( isset($agencyId) && !empty($agencyId) ) {
 
-    $api = new RestClient([
-        'base_url'  => 'https://backend.deqar.eu/webapi/v1/browse/agencies/' . $variable . '/?history=false',
+    // Get the agency data
+    $agency = new RestClient([
+        'base_url'  => 'https://backend.deqar.eu/webapi/v1/browse/agencies/' . $agencyId . '/?history=false',
         'format'    => "json",
         'headers'   => [ 'Authorization' => 'Bearer 03a64a808705672b4fc61f0da1fe28d430250cd4' ],
     ]);
 
+    // Get all countries of the agency
     $countries = new RestClient([
-        'base_url'  => 'https://backend.deqar.eu/webapi/v1/browse/countries/by-agency-focus/' . $variable . '/?limit=999&offset=0',
+        'base_url'  => 'https://backend.deqar.eu/webapi/v1/browse/countries/by-agency-focus/' . $agencyId . '/?limit=999&offset=0',
         'format'    => "json",
         'headers'   => [ 'Authorization' => 'Bearer 03a64a808705672b4fc61f0da1fe28d430250cd4' ],
     ]);
 
+    // Prepare the api data
+    $rAgency        = $agency->get('');
+    $rCountries     = $countries->get('');
 
-    $rApi           = $api->get('');
-
-    if($rApi->info->http_code == 200) {
-        $context['agency'] = $rApi->decode_response();
+    // Check if the agency exists
+    if($rAgency->info->http_code == 200) {
+        $context['agency'] = $rAgency->decode_response();
     } else {
         $context['agency'] = false;
     }
 
-
-    $rCountries     = $countries->get('');
-
+    // Check contries exist
     if($rCountries->info->http_code == 200) {
         $context['agency']->countries = $rCountries->decode_response()->results;
     }
@@ -52,6 +56,5 @@ if ( isset($variable) && !empty($variable) ) {
 
 }
 
-
-
+// Render the twig template.
 Timber::render('agency-single.twig', $context);
