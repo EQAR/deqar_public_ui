c7df4815f939e0c415be6a6e85a132d3255cea2f
Jurgen Oldenburg <info@jold.nl>
2018-03-22 20:08:47 +0100

    Load api data on agency index and agency single views (actualy pages).

--- agency-list.php
+++ agency-list.php
@@ -9,7 +9,23 @@
  * @since    Timber 0.1
  */
 
+include( get_template_directory() . '/classes/RestClient.class.php');
+
 $context = Timber::get_context();
 $context['post'] = new TimberPost();
+
+$api = new RestClient([
+    'base_url' => "https://backend.deqar.eu/webapi/v1/browse/agencies/?limit=999&offset=0",
+    'format' => "json",
+    'headers' => ['Authorization' => 'Bearer 03a64a808705672b4fc61f0da1fe28d430250cd4' ],
+]);
+
+$result = $api->get('agencies' );
+
+if($result->info->http_code == 200) {
+    $context['agencies'] = $result->decode_response();
+} else {
+    $context['agencies'] = [];
+}
 
 Timber::render('agency-list.twig', $context);
--- agency-single.php
+++ agency-single.php
@@ -9,7 +9,33 @@
  * @since    Timber 0.1
  */
 
+include( get_template_directory() . '/classes/RestClient.class.php');
+
 $context = Timber::get_context();
 $context['post'] = new TimberPost();
+
+$variable = $_GET['id'];
+
+if ( isset($variable) && !empty($variable) ) {
+
+    $api = new RestClient([
+        'base_url' => 'https://backend.deqar.eu/webapi/v1/browse/agencies/' . $variable . '/?history=false',
+        'format' => "json",
+        'headers' => ['Authorization' => 'Bearer 03a64a808705672b4fc61f0da1fe28d430250cd4' ],
+    ]);
+
+    $result = $api->get('');
+
+    if($result->info->http_code == 200) {
+        $context['agency'] = $result->decode_response();
+    } else {
+        $context['agency'] = false;
+    }
+
+} else {
+
+    $context['agency'] = false;
+
+}
 
 Timber::render('agency-single.twig', $context);
