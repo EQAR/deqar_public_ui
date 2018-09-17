5cdd96269816dcb3e9a8b185a765ac3a5ad2c48c
Jurgen Oldenburg <info@jold.nl>
2018-09-17 11:46:13 +0200

    Remove eqar api calls and code cleanup.

--- index.php
+++ index.php
@@ -19,12 +19,11 @@ if (!class_exists('Timber')){
 	return;
 }
 
-require_once( get_template_directory() . '/classes/EqarApi.class.php');
-
-$eqarApi = new EqarApi();
+// require_once( get_template_directory() . '/classes/EqarApi.class.php');
+// $eqarApi = new EqarApi();
 
 $context = Timber::get_context();
-$context['post'] = new TimberPost();
+$context['post']         = new TimberPost();
 $context['posts']        = Timber::get_posts();
 $context['pagination']   = Timber::get_pagination();
 // $context['institutions'] = $eqarApi->getInstitutions();
