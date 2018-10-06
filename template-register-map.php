d69b4c7bd41bcd403a9be314e711e3779efb537b
Colin TÃck <ct@tueck.de>
2018-10-06 17:08:59 +0200

    agencies list only, merge other maps & list views

--- template-register-map.php
+++ template-register-map.php
@@ -28,46 +28,29 @@ $context['post'] = new TimberPost();
 
 $allCountries = $eqarApi->getCountries();
 $context['countriesAll'] = $allCountries;
-$showList = false;
-
-if ( isset($_GET['list']) && !empty( $_GET['list'] ) && $_GET['list']  == 'true' ) {
-    $showList = true;
-}
-
-// Countries with agencies map (default map)
-if ( is_page('87') || is_page('420') ) {
-    $context['maptype']   = 'agencies';
-    $context['maptype_list'] = $showList;
-
-    if ( is_page('87') ) {
-        $context['agencies']  = $eqarApi->getAgencies();
-    }
-
-    Timber::render('register-map-page.twig', $context);
-}
-
-// $limit = 999, $offset = 0, $external_qaa = false, $european_approach = false, $eqar_governmental_member = false
 
 // Map Cross-Border Recognition
-elseif ( is_page('426') || is_page('1625') ) {
-    $context['countries'] = $eqarApi->getCountries( 999, 0, $external_qaa = true );
+if ( is_page('426') || is_page('1625') ) {
+//    $context['countries'] = $eqarApi->getCountries( 999, 0, $external_qaa = true );
     $context['maptype']   = 'cross-border';
-    $context['maptype_list'] = $showList;
-    Timber::render('register-map-page.twig', $context);
 }
 
 // Map European Approach for QA of JP
 elseif ( is_page('428') ) {
-    $context['countries'] = $eqarApi->getCountries( 999, 0, false, $european_approach = true );
+//    $context['countries'] = $eqarApi->getCountries( 999, 0, false, $european_approach = true );
     $context['maptype']   = 'eaqejp';
-    $context['maptype_list'] = $showList;
-    Timber::render('register-map-page.twig', $context);
 }
 
 // Map Govermental members
 elseif ( is_page('430') ) {
-    $context['countries'] = $eqarApi->getCountries( 999, 0, false, false, $eqar_governmental_member = true );
+//    $context['countries'] = $eqarApi->getCountries( 999, 0, false, false, $eqar_governmental_member = true );
     $context['maptype']   = 'govermental';
-    $context['maptype_list'] = $showList;
-    Timber::render('register-map-page.twig', $context);
 }
+
+// blank map as default
+else {
+    $context['maptype']   = 'blank';
+}
+
+Timber::render('register-map-page.twig', $context);
+
