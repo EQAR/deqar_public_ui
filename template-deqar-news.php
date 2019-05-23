1cb8cca4c097c5df76792be7ad030aad78dcb480
Colin TÃck <ct@tueck.de>
2019-05-23 22:14:22 +0200

    add category select for DEQAR news template

--- template-deqar-news.php
+++ template-deqar-news.php
@@ -1,18 +1,19 @@
 <?php
 /**
- * Template Name: DEQAR news
+ * Template Name: Custom news | ex DEQAR news
  * Description: show news items from the DEQAR category only
  *
  */
 
+$context = Timber::get_context();
+$context['post']         = new TimberPost();
+
 $query = array(
         'post_type' => 'post',
         'posts_per_page' => 100,
-        'category_name' => 'deqar'
+        'category__in' => $context['post']->get_field('category')
     );
 
-$context = Timber::get_context();
-$context['post']         = new TimberPost();
 $context['posts']        = Timber::get_posts($query);
 
 Timber::render('deqar-news.twig', $context);
